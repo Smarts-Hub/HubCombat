06c4fe3dc2e7df49270689088ce7b3af5742b757
Imhhitt <diegovicentehernandez276@gmail.com>
2025-04-21 16:28:29 +0200

    feat: message on reload

--- src/main/kotlin/dev/smartshub/hubCombat/command/HubCombatCommand.kt
+++ src/main/kotlin/dev/smartshub/hubCombat/command/HubCombatCommand.kt
@@ -2,8 +2,10 @@ package dev.smartshub.hubCombat.command
 
 import dev.smartshub.hubCombat.combat.AllowCombatHandler
 import dev.smartshub.hubCombat.storage.file.FileManager
+import dev.smartshub.hubCombat.util.Msg
 import revxrsal.commands.annotation.Command
 import revxrsal.commands.annotation.Subcommand
+import revxrsal.commands.bukkit.actor.BukkitCommandActor
 import revxrsal.commands.bukkit.annotation.CommandPermission
 
 @Command("hubcombat")
@@ -13,10 +15,11 @@ class HubCombatCommand(
 ) {
 
     @Subcommand("reload")
-    fun reload() {
+    fun reload(actor: BukkitCommandActor) {
         FileManager.get("config")?.reloadFile()
         FileManager.get("lang")?.reloadFile()
         allowCombatHandler.clearCombatAllowed()
+        Msg.send(actor.sender(), "config-reloaded")
     }
 
 }
