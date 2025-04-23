baf3e6f868e5305b17416bc770b876faf4e383f0
Imhhitt <diegovicentehernandez276@gmail.com>
2025-04-23 17:14:41 +0200

    fix: add pdc to items

--- src/main/kotlin/dev/smartshub/hubCombat/item/LoadOutItem.kt
+++ src/main/kotlin/dev/smartshub/hubCombat/item/LoadOutItem.kt
@@ -1,10 +1,13 @@
 package dev.smartshub.hubCombat.item
 
+import dev.smartshub.hubCombat.HubCombat
 import dev.smartshub.hubCombat.util.Msg
 import org.bukkit.Material
+import org.bukkit.NamespacedKey
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
 
 data class LoadOutItem(
     val slot: Int,
@@ -12,7 +15,8 @@ data class LoadOutItem(
     val name: String?,
     val lore: List<String>?,
     val enchantments: Map<Enchantment, Int>,
-    val flags: List<ItemFlag>
+    val flags: List<ItemFlag>,
+    val plugin: HubCombat
 ){
 
     fun toItemStack(): ItemStack {
@@ -35,6 +39,9 @@ data class LoadOutItem(
             meta.addItemFlags(it)
         }
 
+        // Adding PersistentDataContainer to identify the item
+        val key = NamespacedKey(plugin, "hubCombatItem")
+        meta.persistentDataContainer.set(key, PersistentDataType.STRING, "hubCombatItem")
         item.itemMeta = meta
         return item
     }
