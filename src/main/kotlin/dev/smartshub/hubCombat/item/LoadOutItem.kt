9329b33c64b3351973fa785880cb79f514cf8695
Imhhitt <diegovicentehernandez276@gmail.com>
2025-04-23 17:01:48 +0200

    feat: method to give to players directly

--- src/main/kotlin/dev/smartshub/hubCombat/item/LoadOutItem.kt
+++ src/main/kotlin/dev/smartshub/hubCombat/item/LoadOutItem.kt
@@ -1,8 +1,10 @@
 package dev.smartshub.hubCombat.item
 
+import dev.smartshub.hubCombat.util.Msg
 import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.ItemFlag
+import org.bukkit.inventory.ItemStack
 
 data class LoadOutItem(
     val slot: Int,
@@ -11,4 +13,30 @@ data class LoadOutItem(
     val lore: List<String>?,
     val enchantments: Map<Enchantment, Int>,
     val flags: List<ItemFlag>
-)
+){
+
+    fun toItemStack(): ItemStack {
+        val item = ItemStack(material)
+        val meta = item.itemMeta ?: return item
+
+        name?.let {
+            meta.displayName(Msg.parse(it))
+        }
+
+        lore?.let {
+            meta.lore(Msg.parseList(it))
+        }
+
+        enchantments.forEach { (enchant, level) ->
+            meta.addEnchant(enchant, level, true)
+        }
+
+        flags.forEach {
+            meta.addItemFlags(it)
+        }
+
+        item.itemMeta = meta
+        return item
+    }
+}
+
